d0c6e6a03e20f716d3fb3292de3f66b05c0aabf6
EHINDLE <ehindle@ntlworld.com>
2020-01-31 20:49:19 +0000

    Remove requires

--- lms_php/struct/player/invite-player.php
+++ lms_php/struct/player/invite-player.php
@@ -4,8 +4,6 @@ $myPath = '../../';
 require $myPath . 'includes/db_connect.php';
 require $myPath . 'includes/functions.php';
 require $myPath . 'includes/formkey.class.php';
-require $myPath . 'includes/mail-util.php';
-require $myPath . 'struct/game/game-functions.php';
 require $myPath . 'struct/player/player-functions.php';
 
 sec_session_start();
