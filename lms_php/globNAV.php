c5602e1a62e272c0b5647791fb9eebcd70689ffc
EHINDLE <ehindle@ntlworld.com>
2020-02-02 01:41:20 +0000

    Improve globNAV collape

--- lms_php/globNAV.php
+++ lms_php/globNAV.php
@@ -6,7 +6,7 @@ $devlevelneeded = 901;
 echo '
 		   <section>
 
-			<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+			<nav role="navigation" class="navbar navbar-default navbar-fixed-top navbar-expand-lg">
 					<div class="container-fluid">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
@@ -33,16 +33,17 @@ if ($currentlevel == $devlevelneeded) {
 								<li class="nav-item"><a href="' . $myPath . 'menus/testmenu.php">Test</a></li>';
 }
 echo '                      </ul>
-							<ul class="nav navbar-nav navbar-right">
-                                <li class="nav-item"><a href="#">Period: ' . $_SESSION['currentweek'] . '/' . $_SESSION['currentseason'] . '</a></li>
-								<li class="nav-item">
-                                    <form class="form-inline" role="form" name ="myaccount" method="post" action="' . $myPath . 'struct/player/myaccount.php">
-                                        <span class="glyphicon glyphicon-user"></span>' . $key .
-				                        '<input id="submit" name="submit" type="submit" value=" ' . $_SESSION['nickname'] . '" class="navbar nav-button">
-                                    </form>
-                                </li>
-								<li class="nav-item"><a href="' . $myPath . 'logout.php"><span class="glyphicon glyphicon-log-in"></span> Sign out</a></li>
-							</ul>
+
+<ul class="nav navbar-nav navbar-right" >';
+if ($currentlevel != $adminlevelneeded && $currentlevel != $devlevelneeded) {
+    echo '                        <li class="nav-item">    <a class="nav-link" href="#">Period: ' . $_SESSION['currentweek'] . '/' . $_SESSION['currentseason'] . '</a></li>';
+}
+echo '                            <li class="nav-item">    <form class="form-inline" role="form" name ="myaccount" method="post" action="' . $myPath . 'struct/player/myaccount.php">
+                                <span class="glyphicon glyphicon-user"></span>' . $key . '<input id="submit" name="submit" type="submit" value="'. $_SESSION['nickname'] . '" class="navbar nav-button">
+                            </form></li>
+                        <li class="nav-item">    <a  class="nav-link" href="' . $myPath . 'logout.php"><span class="glyphicon glyphicon-log-in"></span> Sign out</a></li>
+</ul>
+
 						</div>
 					</div>
 				</nav>
