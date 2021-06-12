0488f26ab2cc55f5a6304c7673f2e7e1ec393b9b
EHINDLE <ehindle@ntlworld.com>
2021-06-12 14:21:48 +0100

    Change testmenu to use Adam's style Tidy and add back button to view log

--- lms_php/menus/testmenu.php
+++ lms_php/menus/testmenu.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * HINDLEWARE
+ * Copyright (C) 2021 Eric Hindle. All rights reserved.
+ */
 $myPath = '../';
 require $myPath . 'includes/db_connect.php';
 require $myPath . 'includes/functions.php';
@@ -14,86 +18,55 @@ if (login_check($mypdo) == true && $_SESSION['retaccess'] == 901) {
     echo '
 		<!doctype html>
 		<html>
-			<head>
-				
-			    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-			    <meta charset="UTF-8">
-			    
-			    <title>Development</title>
-			    
-			    <meta name="viewport" content="width=device-width, initial-scale=1">
-			    <link rel="stylesheet" href="' . $myPath . 'css/bootstrap.min.css">
-			    <link rel="stylesheet" href="' . $myPath . 'css/rethome.css">
-			    <script src="' . $myPath . 'js/jquery.js"></script>
-			    <script src="' . $myPath . 'js/bootstrap.min.js"></script>
-			</head>
-
+            <head>
+                <meta charset="UTF-8">
+                <title>View Logfile</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" href="' . $myPath . '../css/style.css" type="text/css">
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        	</head>
 			<body>';
     include $myPath . 'globNAV.php';
-    echo '
-				<section id="homeSection">
-			    <br><br>
-			        <div class="container">
-			            <div class="row">
-			                <div class="col-md-12">
-			                    <h1><strong>Development and Testing</strong></h1>
-			                    <br>
-			                </div>
-			            </div>
-			            <div class="row">
-			            	<div class="col-sm-4">
-			                    <div class="tile red">
-			                    	<a href="' . $myPath . 'testing/testforms/emailtest.php">
-			                    		<h3 class="title" >Email test</h3>
-			                        </a>	
-			          			</div>
-			                </div>
-			            	<div class="col-sm-4">
-			                    <div class="tile blue">
-			                    	<a href="' . $myPath . 'testing/testforms/viewlog.php">
-			                    		<h3 class="title" >View Log</h3>
-			                        </a>	
-			          			</div>
-			                </div>
-			                <div class="col-sm-4">
-			                    <div class="tile green">
-                                    <h3 class="title" >Read a JSON file</h3>
-				                	<form role="form" name ="json" method="post" action="' . $myPath . 'testing/testforms/jsontest.php">';
-    $html .= $key;
-    $html .= '                          <div class="form-group " style="margin-left:16px;margin-right:16px">
-					                        <input type="text" class="form-control" id="filename" name="filename" placeholder="file name">
-					                    </div>
-					                    <div class="form-group" style="margin-left:16px;margin-right:16px">
-					                    	<br>
-					                        <input id="submit2" name="submit" type="submit" value="Submit" class="btn btn-primary">
-					                    </div>
-					                </form>
-			          			</div>
-			                </div>
-			      		</div>
-                        <div class="row">
-			            	<div class="col-sm-4">
-			                    <div class="tile orange">
-			                    	<a href="' . $myPath . 'testing/testforms/encryptiontest.php">
-			                    		<h3 class="title" >Encryption test</h3>
-			                        </a>	
-			          			</div>
-			                </div>
+    $html .= '
+			     <div class="container">
+                    <div class="box" style="padding:1em;">
+                        <h2>Development and Testing</h2>
+                    </div>
+                    <div class="box" style="padding:1em;padding-left:10%;padding-right:10%;margin:10px;">
+                        <div class="btn" style="padding:3px;margin:3px;width:100%;">
+                            <a href="' . $myPath . 'testing/testforms/viewlog.php">
+                                <h3 style="color:white;">View Log</h3>
+                            </a>	
+                        </div>	
+                        <div class="btn" style="padding:3px;margin:3px;width:100%;">
+                            <a href="' . $myPath . 'testing/testforms/emailtest.php">
+                                <h3 style="color:white;">Email test</h3>
+                            </a>	
                         </div>
-			      		<div class="row">
-							<br>
-							<div class="col-xs-6">
-								<a href="' . $myPath . 'menus/home.php" class="btn btn-primary btn-lg push-to-bottom" role="button">Back</a>
-								<br>
-							</div>
-						</div>
-			      		<br><br><br><br>
+                        <div class="btn" style="padding:3px;margin:3px;width:100%;">
+                            <a href="' . $myPath . 'testing/testforms/encryptiontest.php">
+                                <h3 style="color:white;" >Encryption test</h3>
+                            </a>	
+                        </div>
+                    </div>
+                    <div class="box" style="padding:1em;margin:10px;">
+                        <h3>Read a JSON file</h3>
+                        <form role="form" name ="json" method="post" action="' . $myPath . 'testing/testforms/jsontest.php">';
+    $html .= $key;
+    $html .= '              <div class="form-group " style="margin-left:16px;margin-right:16px">
+                                <input type="text" class="form-control" id="filename" name="filename" placeholder="file name">
+					        </div>
+                            <div class="form-group" style="margin-left:16px;margin-right:16px">
+					            <input id="submit2" name="submit" type="submit" value="Submit" class="btn" style="margin:10px;padding:5px;width:50%;">
+					        </div>
+					    </form>
+			        </div>
+                    <div style="padding:2em;">
+                        <a href="' . $myPath . 'menus/home.php" class="btn" style="padding:15px;" role="button">Back</a>
+                    </div>
 		    	</div>
-		    </section>
-		</body>
-	</html>
-
-		';
+            </body>
+        </html>';
     echo $html;
 } else {
     header('Location: ' . $myPath . 'index.php?error=1');
--- lms_php/testing/testforms/viewlog.php
+++ lms_php/testing/testforms/viewlog.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * HINDLEWARE
+ * Copyright (C) 2021 Eric Hindle. All rights reserved.
+ */
 $myPath = '../../';
 require_once $myPath . 'includes/db_connect.php';
 require_once $myPath . 'includes/functions.php';
@@ -9,53 +13,44 @@ $formKey = new formKey();
 $key = $formKey->outputKey();
 $_SESSION['currentweek'] = get_global_value('currweek');
 $_SESSION['currentseason'] = get_global_value('currseason');
-$_SESSION['selectweek'] = get_global_value('selectweek');
 $_SESSION['matchweek'] = $_SESSION['currentseason'] . $_SESSION['currentweek'];
-        $logfilename = "../..//logs/lml-log-" . $_SESSION['matchweek'] . ".log";
-            $logfile = fopen($logfilename, "r");
-            $contents = '';
-            if ($logfile) {
-                while (($line = fgets($logfile)) !== false) {
-                  $contents .= $line . '<br/>';
-                }
-                
-                fclose($logfile);
-            } 
-            $html = ' <!doctype html>
+$logfilename = "../../logs/lml-log-" . $_SESSION['matchweek'] . ".log";
+$logfile = fopen($logfilename, "r");
+$contents = '';
+if ($logfile) {
+    while (($line = fgets($logfile)) !== false) {
+        $contents .= $line . '<br/>';
+    }
+
+    fclose($logfile);
+}
+$html = ' <!doctype html>
             <html>
             <head>
                 <meta charset="UTF-8">
                 <title>View Logfile</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="stylesheet" href="../../../css/style.css" type="text/css">
+                <link rel="stylesheet" href="' . $myPath . '../css/style.css" type="text/css">
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         	</head>
                     
-        	<body  >';
-            $html .= '
-			        <div class="container">
-			        	<div class="box" style="width:350px;padding: 1.5em;height: 140px;">
-			                    <h1 style="text-align:center;"><strong>View Logfile</strong></h1>
-<h3 style="text-align:center; "><strong>';
-                $html .= $logfilename;
-                $html .= '</strong></h3>
-			                    <br> </div>
-<div class="box" style="width:90%;padding: 1em;text-align:left"> '. $contents . ' </div>
-			                </div>
-			            ';
-
-            $html .= '
-			    	</div>
+        	<body>';
+$html .= '
+			     <div class="container">
+			         <div class="box" style="width:350px;padding: 0.5em;height: 80px;">
+			             <h3 style="text-align:center;">View Logfile</h3>
+                         <h5 style="text-align:center; ">';
+$html .= $logfilename;
+$html .= '               </h5>
+			         </div>
+                     <div class="box" style="width:90%;padding: 1em;text-align:left;margin:10px"> ' . $contents . ' </div>';
+$html .= '           <div style="padding:2em;">
+						  <a href="' . $myPath . 'menus/testmenu.php" class="btn" style="padding:15px;" role="button">Back</a>
+				     </div>
+			    </div>
 			</body>
-		</html>
-                    
-		';
+		</html>';
 
-            echo $html;
-
-    
-
-
-
+echo $html;
 
 ?>   
