da7c9611d059e29638c7a25bf6b7d424cdb0f4a0
Eric <ekky1955@gmail.com>
2022-07-30 16:15:15 +0100

    Duplicate abbr check

--- lms_php/struct/team/process-edit-team.php
+++ lms_php/struct/team/process-edit-team.php
@@ -38,71 +38,84 @@ if (login_check($mypdo) == true && $access > 900) {
                     $teamcount = $teamquery->rowCount();
                     $alerttext = "";
                     if ($teamcount > 0) {
-                        remove_abbr_for_team($teamid);
                         $abbrlist = explode(', ',$teamabbr);
+                        $dupct = 0;
                         foreach ($abbrlist as $abbr) {
-                            insert_team_abbr($teamid, $abbr);
+                            $dupct += check_dup_abbr($teamid, $abbr);
                         }
-                        
-                        date_default_timezone_set('Europe/London');
-                        $phptime = time();
-                        $mysqltime = date("Y-m-d H:i:s", $phptime);
-                        $upsql = "UPDATE lms_team SET lms_team_name = :teamname, lms_team_active = :active, lms_team_abbr = :abbr WHERE lms_team_id = :id";
-                        $upquery = $mypdo->prepare($upsql);
-                        $upquery->bindParam(':id', $teamid, PDO::PARAM_INT);
-                        $upquery->bindParam(':teamname', $teamname);
-                        $upquery->bindParam(':abbr', $abbrlist[0]);
-                        $upquery->bindParam(':active', $myactive, PDO::PARAM_INT);
-                        $upquery->execute();
-                        $upcount = $upquery->rowCount();
-                        if ($upcount > 0) {
-                            $alerttext = "Details updated successfully. ";
-                        }
-                        
-                        $leaguesql = "SELECT lms_league_name, lms_league_id FROM lms_league";
-                        $leaguequery = $mypdo->prepare($leaguesql);
-                        $leaguequery->execute();
-                        $leaguecount = $leaguequery->rowCount();
-                        if ($leaguecount > 0) {
-                            $leaguefetch = $leaguequery->fetchAll(PDO::FETCH_ASSOC);
-                            foreach ($leaguefetch as $rs) {
-                                $rmvid = $rs['lms_league_id'];
-                                $postrmv = $_POST["rmv-" . $rmvid];
-                                if ($postrmv == "true") {
-                                    $sqlrmvteamleague = "DELETE FROM lms_league_team WHERE lms_league_team_league_id = :leagueid AND lms_league_team_team_id = :teamid";
-                                    $sqlrmvteamleague = $mypdo->prepare($sqlrmvteamleague);
-                                    $sqlrmvteamleague->execute(array(
-                                        ':teamid' => $teamid,
-                                        ':leagueid' => $rmvid
-                                    ));
-                                    $leaguermv = $sqlrmvteamleague->rowCount();
+                        if ($dupct == 0){
+                            $abbrct = remove_abbr_for_team($teamid);
+                            foreach ($abbrlist as $abbr) {
+                                $abbrct += insert_team_abbr($teamid, $abbr);
+                            }
+                            if ($abbrct > 0) {
+                                 $alerttext .= "Abbr added to team.";
+                            }
+                            date_default_timezone_set('Europe/London');
+                            $phptime = time();
+                            $mysqltime = date("Y-m-d H:i:s", $phptime);
+                            $upsql = "UPDATE lms_team SET lms_team_name = :teamname, lms_team_active = :active, lms_team_abbr = :abbr WHERE lms_team_id = :id";
+                            $upquery = $mypdo->prepare($upsql);
+                            $upquery->bindParam(':id', $teamid, PDO::PARAM_INT);
+                            $upquery->bindParam(':teamname', $teamname);
+                            $upquery->bindParam(':abbr', $abbrlist[0]);
+                            $upquery->bindParam(':active', $myactive, PDO::PARAM_INT);
+                            $upquery->execute();
+                            $upcount = $upquery->rowCount();
+                            if ($upcount > 0) {
+                                $alerttext = "Details updated successfully. ";
+                            }
+                            
+                            $leaguesql = "SELECT lms_league_name, lms_league_id FROM lms_league";
+                            $leaguequery = $mypdo->prepare($leaguesql);
+                            $leaguequery->execute();
+                            $leaguecount = $leaguequery->rowCount();
+                            if ($leaguecount > 0) {
+                                $leaguefetch = $leaguequery->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($leaguefetch as $rs) {
+                                    $rmvid = $rs['lms_league_id'];
+                                    $postrmv = $_POST["rmv-" . $rmvid];
+                                    if ($postrmv == "true") {
+                                        $sqlrmvteamleague = "DELETE FROM lms_league_team WHERE lms_league_team_league_id = :leagueid AND lms_league_team_team_id = :teamid";
+                                        $sqlrmvteamleague = $mypdo->prepare($sqlrmvteamleague);
+                                        $sqlrmvteamleague->execute(array(
+                                            ':teamid' => $teamid,
+                                            ':leagueid' => $rmvid
+                                        ));
+                                        $leaguermv = $sqlrmvteamleague->rowCount();
+                                    }
                                 }
                             }
-                        }
-                        $leagueadded = 0;
-                        if ($addleague == 'true') {
-                            $leagueId = sanitize_int($_POST['leagueid']);
-                            $sqladdteamleague = "INSERT INTO lms_league_team (lms_league_team_league_id, lms_league_team_team_id) VALUES (:leagueid, :teamid)";
-                            $sqladdteamleague = $mypdo->prepare($sqladdteamleague);
-                            $sqladdteamleague->execute(array(
-                                ':teamid' => $teamid,
-                                ':leagueid' => $leagueId
-                            ));
-                            $leagueadded = $sqladdteamleague->rowCount();
-                            if ($leagueadded > 0) {
-                                $alerttext .= "Team successfully added to league.";
+                            $leagueadded = 0;
+                            if ($addleague == 'true') {
+                                $leagueId = sanitize_int($_POST['leagueid']);
+                                $sqladdteamleague = "INSERT INTO lms_league_team (lms_league_team_league_id, lms_league_team_team_id) VALUES (:leagueid, :teamid)";
+                                $sqladdteamleague = $mypdo->prepare($sqladdteamleague);
+                                $sqladdteamleague->execute(array(
+                                    ':teamid' => $teamid,
+                                    ':leagueid' => $leagueId
+                                ));
+                                $leagueadded = $sqladdteamleague->rowCount();
+                                if ($leagueadded > 0) {
+                                    $alerttext .= "Team successfully added to league.";
+                                }
                             }
-                        }
-                        if ($upcount > 0 || $leagueadded > 0) {
-                            $html .= "<script>
-												alert('".$alerttext."');
-												window.location.href='team-main.php';
-											</script>";
+                            if ($upcount > 0 || $leagueadded > 0 || $abbrct > 0) {
+                                $html .= "<script>
+    											alert('".$alerttext."');
+    											window.location.href='team-main.php';
+    									  </script>";
+                            } else {
+                                $html .= "<script>
+    										alert('Details not altered.');
+    										window.location.href='team-main.php';
+    									</script>";
+                            }
                         } else {
                             $html .= "<script>
-										alert('Details not altered.');
-										window.location.href='team-main.php';
-									</script>";
+    										alert('Duplicate abbreviation(s). Not updated.');
+    										window.location.href='team-main.php';
+    							     </script>";
                         }
                     } else {
                         $html .= "<script>
--- lms_php/struct/team/team_functions.php
+++ lms_php/struct/team/team_functions.php
@@ -7,6 +7,18 @@
 $myPath = '../../';
 require $myPath . 'includes/db_connect.php';
 
+function check_dup_abbr($teamid, $abbr) {
+    global $mypdo;
+    $abbrsql = "SELECT * FROM lms_team_abbr WHERE lms_team_abbr_abbr = :abbr AND lms_team_abbr_team_id <> :team;";
+    $abbrquery = $mypdo->prepare($abbrsql);
+    $abbrquery->execute(array(
+        ':team' => $teamid,
+        ':abbr' => $abbr
+    ));
+    $abbrquery->fetchAll(PDO::FETCH_ASSOC);
+    return  $abbrquery->rowCount();
+}
+
 function remove_abbr_for_team($teamid) {
     global $mypdo;
     $abbrsql = "DELETE FROM lms_team_abbr WHERE lms_team_abbr_team_id = :team;";
@@ -14,13 +26,12 @@ function remove_abbr_for_team($teamid) {
     $abbrquery->execute(array(
         ':team' => $teamid
     ));
-    
+    return  $abbrquery->rowCount();
 }
 
 function insert_team_abbr($teamid, $abbr)
 {
     global $mypdo;
-
     $abbrsql = "INSERT INTO lms_team_abbr (lms_team_abbr_abbr, lms_team_abbr_team_id)
         VALUES (:abbr, :team);";
         $abbrquery = $mypdo->prepare($abbrsql);
@@ -28,8 +39,7 @@ function insert_team_abbr($teamid, $abbr)
             ':team' => $teamid,
             ':abbr' => $abbr
         ));
-    
-     return;
+        return $abbrquery->rowCount();
 }
 
 function get_team_by_abbr($abbr){
