4eb92cc97c7b129ab65d9eed3cdb436c5f64a863
Eric <ekky1955@gmail.com>
2022-08-12 11:28:10 +0100

    Game list next pick changes

--- lms_php/menus/game-list.php
+++ lms_php/menus/game-list.php
@@ -12,7 +12,7 @@ $gamequery->bindParam(':player', $playerid, PDO::PARAM_INT);
 $gamequery->execute();
 $gamefetch = $gamequery->fetchAll(PDO::FETCH_ASSOC);
 $currentPage = 'games';
-$picksql = "SELECT lms_team_name, lms_match_date  FROM v_lms_player_picks WHERE lms_pick_player_id = :player and lms_pick_game_id = :game and lms_match_weekno = :matchwk LIMIT 1";
+$picksql = "SELECT lms_team_name, lms_match_date, lms_week  FROM v_lms_player_picks WHERE lms_pick_player_id = :player and lms_pick_game_id = :game and lms_match_weekno = :matchwk LIMIT 1";
 $key = $formKey->outputKey();
 $html = '';
 if (login_check($mypdo) == true) {
@@ -37,20 +37,24 @@ if (login_check($mypdo) == true) {
         $pickquery = $mypdo->prepare($picksql);
         $pickquery->bindParam(':player', $playerid, PDO::PARAM_INT);
         $pickquery->bindParam(':game', $gameid, PDO::PARAM_INT);
+        /* Get this week's pick */
         $pickquery->bindParam(':matchwk', $_SESSION['matchweek']);
         $pickquery->execute();
         $matchcount = $pickquery->rowCount();
         $matchweekpick = $pickquery->fetch(PDO::FETCH_ASSOC);
+        /* Get next week's pick */
         $pickquery->bindParam(':matchwk', $_SESSION['selectweekkey']);
         $pickquery->execute();
-        $selectweekpick = $pickquery->fetch(PDO::FETCH_ASSOC);
         $selectcount = $pickquery->rowCount();
-        $pickcount = $matchcount + $selectcount;
-        $currentpick = 'No Pick';
+        $selectweekpick = $pickquery->fetch(PDO::FETCH_ASSOC);
+
+        $showthisweekspick = $rs['lms_game_status'] > 1;
+        $shownextweekspick = $rs['lms_game_player_status'] == 1;
+
         $thispick = 'No Pick';
-        $nextpick = '';
+        $nextpick = '(make a pick now)';
+
         $rowcolor = 'black';
-        $selcolor = 'black';
         $playercolor = 'status-playing';
         switch ($rs['lms_game_status']) {
             case 1:
@@ -66,31 +70,28 @@ if (login_check($mypdo) == true) {
                 $rowcolor = 'cancelled';
                 break;
         }
+        /*
+         * If player is out (or left the game) this week - show the team
+         */
+        $displaynextweek = '';
         if ($rs['lms_game_player_status'] == 2 or $rs['lms_game_player_status'] == 3) {
-            $playercolor = $rs['lms_game_player_status'] == 2 ? 'status-out' : 'status-playing';
-            if ($rs['lms_game_player_status'] == 2){
+            $playercolor = $rs['lms_game_player_status'] == 2 ? 'status-out' : 'cancelled';
+            if ($rs['lms_game_player_status'] == 2) {
                 if ($matchcount > 0) {
                     $thispick = $matchweekpick['lms_team_name'];
                 }
             }
         } else {
-            if ($pickcount > 0) {
-                $newline = '';
-                if ($pickcount == 2) {
-                    $newline = '</br>';
-                }
+            if ($rs['lms_game_start_wkno'] > $_SESSION['selectweekkey']) {
+                $selectionstart = get_selection_start_date($rs['lms_game_start_wkno']);
+                $nextpick = 'Team selection begins ' . date_format(date_create($selectionstart), 'd M Y');
+            } else {
                 if ($matchcount > 0) {
                     $thispick = $matchweekpick['lms_team_name'];
                 }
                 if ($selectcount > 0) {
+                    $displaynextweek = '(Wk.' . sprintf('%02d', $selectweekpick['lms_week']) . ')';
                     $nextpick = $selectweekpick['lms_team_name'];
-                }
-                $currentpick = $thispick . $newline . $nextpick;
-            } else {
-                if ($rs['lms_game_start_wkno'] <= $_SESSION['matchweek']) {
-                    $currentpick = '(waiting)';
-                    $thispick = '(waiting)';
-                    $selcolor = 'crimson';
                 }
             }
         }
@@ -121,19 +122,38 @@ if (login_check($mypdo) == true) {
                             <div class="table-columnTitle">Players</div>
                             <div>' . $rs['lms_game_still_active'] . ' / ' . $rs['lms_game_total_players'] . '</div>
                             </td>
-                        </tr>
-                        <tr>
+                        </tr>';
+        if ($showthisweekspick) {
+            $html .= '  <tr>
+       
+
                             <td colspan="2" class="your-pick-table">
-                                <div class="table-columnTitle">This Weeks Pick:</div>
+                                <div class="table-columnTitle">This Week\'s Pick:</div>
                                 <div><h3><b>' . $thispick . '</b></h3></div>';
-        if ($selectcount > 0) {
-            $html .= '          <div class="table-columnTitle">Fixture: (' . date_format(date_create($selectweekpick['lms_match_date']), 'd M Y') . ')</div> ';
+            if ($matchcount > 0) {
+                $html .= '          <div class="table-columnTitle">Fixture: (' . date_format(date_create($matchweekpick['lms_match_date']), 'd M Y') . ')</div> ';
+            }
+            $html .= '          </td>
+                         </tr>';
         }
-        $html .= '          </td>
+        $html .= '            <tr>
+                            <td colspan="2" class="your-pick-table">';
+        if ($shownextweekspick) {
+
+            $html .= '          <div class="table-columnTitle">Next Week\'s Pick ' . $displaynextweek . ':</div>
+                                <div><h3><b>' . $nextpick . '</b></h3></div>';
+            if ($selectcount > 0) {
+                $html .= '      <div class="table-columnTitle">Fixture: (' . date_format(date_create($selectweekpick['lms_match_date']), 'd M Y') . ')</div>';
+            }
+            $html .= '      </td>';
+        }
+        $html .= '                                
                             <td>
                             <img style="width:30px" src="' . $myPath . 'img/icons/PickButton.svg">
                             </td>
                         </tr>
+
+
                     </table>
                     <div class="status-bar ' . $playercolor . '">Your Status: ' . $rs['lms_game_player_status_text'] . '</div>
                 </div>
@@ -151,4 +171,17 @@ if (login_check($mypdo) == true) {
 } else {
     header('Location: ' . $myPath . 'index.php?error=1');
 }
+
+function get_selection_start_date($gamestartweek)
+{
+    global $mypdo;
+    $previousweek = $gamestartweek - 1;
+    $weeksql = "SELECT lms_week_start FROM lms_week WHERE lms_week_no = :weekno LIMIT 1";
+    $weekquery = $mypdo->prepare($weeksql);
+    $weekquery->bindParam(":weekno", $previousweek, PDO::PARAM_INT);
+    $weekquery->execute();
+    $weekfetch = $weekquery->fetch(PDO::FETCH_ASSOC);
+    return $weekfetch['lms_week_start'];
+}
+
 ?> 
